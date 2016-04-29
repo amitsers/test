2b9a4b383dbe315fc89ad72b7e48df5b389a9c20
Amit Sinha <amitsinha559@gmail.com>
2016-04-29 17:01:15 +0530

    asdasd

---
+++ telso001/save-contact.php
@@ -0,0 +1,31 @@
+<?php
+	if(isset($_POST['mobile_no'])) {
+
+		$mobile_no = $_POST['mobile_no'];
+		$ip = "";
+		if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+			$ip = $_SERVER['HTTP_CLIENT_IP'];
+		} elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+			$ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+		} else {
+			$ip = $_SERVER['REMOTE_ADDR'];
+		}
+
+		$servername = "localhost";
+		$username = "username";
+		$password = "password";
+		$dbname = "myDB";
+
+		$conn = new mysqli($servername, $username, $password, $dbname);
+
+		$sql = "INSERT INTO track_mobile (mobile, ip) VALUES ('$mobile_no', '$ip')";
+		$conn->query($sql);
+		if ($conn->query($sql) === TRUE) {
+		    echo 1;
+		} else {
+		    echo 2;
+		}
+
+		$conn->close();
+	}
+?>
